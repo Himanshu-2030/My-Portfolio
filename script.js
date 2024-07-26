5bef90fb60eb278f84824e121d29660ae1890e28
Himanshu Nirmalkar <114094406+Himanshu-2030@users.noreply.github.com>
2024-07-26 16:41:31 +0530

    responsive update timeline

--- script.js
+++ script.js
@@ -7,22 +7,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }, index * 100); // Stagger animation timing
     });
 });
-
-// circle text 
-
-const circleType = new CircleType(
-    document.getElementById("rotated")
-).radius(80);
-
-$(window).scroll(function() {
-    var offset = $(window).scrollTop();
-    offset = offset * 0.4;
-
-    $(".circular-text").css({
-        "-moz-transform": "rotate(" + offset + "deg)",
-        "-webkit-transform": "rotate(" + offset + "deg)",
-        "-o-transform": "rotate(" + offset + "deg)",
-        "-ms-transform": "rotate(" + offset + "deg)",
-        transform: "rotate(" + offset + "deg)"
-    });
-});
